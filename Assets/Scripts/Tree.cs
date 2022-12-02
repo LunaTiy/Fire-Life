9deaf940d2dda49a7be2a59c7282dbce0666d556
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2022-12-02 18:22:00 +0300

    Add grab available tree

---
+++ Assets/Scripts/Tree.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Tree : MonoBehaviour, IGrabAvailable
+{
+    [SerializeField] private GameObject _logPrefab;
+    
+    public GameObject GetItem()
+    {
+        Destroy(gameObject);
+
+        GameObject log = Instantiate(_logPrefab, transform.position, Quaternion.identity);
+        return log;
+    }
+}
