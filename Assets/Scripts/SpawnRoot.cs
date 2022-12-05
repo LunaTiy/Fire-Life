37bd616efbbfac4d68ef357696c556833ec53c41
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2022-12-05 19:00:02 +0300

    Add spawners

---
+++ Assets/Scripts/SpawnRoot.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SpawnRoot : MonoBehaviour
+{
+    [ContextMenu("Spawn all objects")]
+    private void SpawnAll()
+    {
+        var spawners = GetComponentsInChildren<Spawner>();
+
+        foreach (Spawner spawner in spawners)
+            spawner.Spawn();
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[SuppressMessage("ReSharper", "VirtualMemberNeverOverridden.Global")]
+public abstract class Spawner : MonoBehaviour
+{
+    [SerializeField] protected int minTimeReload;
+    [SerializeField] protected int maxTimeReload;
+    [SerializeField] protected int maxCountObjects;
+
+    [SerializeField] protected GameObject[] _prefabs;
+
+    protected virtual int CountObjects => transform.childCount;
+    
+    protected bool canSpawn = true;
+
+    protected void Start()
+    {
+        if (minTimeReload > maxTimeReload)
+            throw new ArgumentException($"Min time: {minTimeReload} is greater max time: {maxTimeReload}");
+
+        StartCoroutine(SpawnRoutine());
+    }
+
+    protected virtual IEnumerator SpawnRoutine()
+    {
+        while (canSpawn)
+        {
+            Spawn();
+            yield return new WaitForSeconds(Random.Range(minTimeReload, maxTimeReload));
+        }
+
+        yield return null;
+    }
+
+    [ContextMenu("Spawn Object")]
+    public virtual void Spawn()
+    {
+        if (CountObjects >= maxCountObjects) return;
+        
+        if (_prefabs == null || _prefabs.Length == 0)
+            throw new NullReferenceException("Prefab null reference exception");
+                
+        GameObject prefabForInstantiate = _prefabs[Random.Range(0, _prefabs.Length)];
+
+        GameObject obj = Instantiate(prefabForInstantiate, Vector3.zero, Quaternion.identity, transform);
+        ResetLocalTransform(obj.transform);
+    }
+    
+    protected virtual void ResetLocalTransform(Transform transformObj)
+    {
+        transformObj.localPosition = Vector3.zero;
+        transformObj.localRotation = new Quaternion(0, Random.Range(0, 360), 0, 0);
+        transformObj.localScale = Vector3.one;
+    }
+}
