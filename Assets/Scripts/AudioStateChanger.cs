ce711fd3a647376f729b6fc32068dae52f08e419
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2022-12-07 17:32:59 +0300

    Add audio listener state changer

---
+++ Assets/Scripts/AudioStateChanger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class AudioStateChanger : MonoBehaviour, IPointerUpHandler
+{
+    [SerializeField] private AudioListener _audioListener;
+    [SerializeField] private Image _audioStateChanger;
+    [SerializeField] private Sprite _soundOnState;
+    [SerializeField] private Sprite _soundOffState;
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        bool isEnable = !_audioListener.enabled;
+        
+        _audioListener.enabled = isEnable;
+        _audioStateChanger.sprite = isEnable ? _soundOnState : _soundOffState;
+    }
+}
